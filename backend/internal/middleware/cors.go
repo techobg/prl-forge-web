de6f0a0c422ca4409d922c97aca5fd8da9d3d4aa
Quality <Quality@TCB.local>
2026-07-02 16:19:05 +0300

    Fix CORS

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -5,7 +5,7 @@ import "net/http"
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 
